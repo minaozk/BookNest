a2382cac4c9dec5a72d2562f3cec301aa6f8da08
minaozk <80757513+minaozk@users.noreply.github.com>
2024-04-20 18:23:41 +0300

    Add project files.

---
+++ BookNest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookNest.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
